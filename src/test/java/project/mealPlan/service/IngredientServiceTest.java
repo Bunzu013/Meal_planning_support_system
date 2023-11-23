ff01aa07945ba514d767fe60981c1de5bc0cf6fa
Bunzu013 <83347605+Bunzu013@users.noreply.github.com>
2023-11-23 23:36:10 +0100

    test ingredients

--- src/test/java/project/mealPlan/service/IngredientServiceTest.java
+++ src/test/java/project/mealPlan/service/IngredientServiceTest.java
@@ -1,20 +1,11 @@
 package project.mealPlan.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
 class IngredientServiceTest {
 
-    @Test
-    void addToAllergens() {
-    }
-
-    @Test
-    void getUserAllergens() {
-    }
-
-    @Test
-    void addNewIngredient() {
-    }
 }
