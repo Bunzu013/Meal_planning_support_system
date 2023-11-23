08834239b6fa633e005abd9daae4929fde574b99
Bunzu013 <83347605+Bunzu013@users.noreply.github.com>
2023-11-23 23:37:13 +0100

    recipe meal - 3

--- src/test/java/project/mealPlan/service/RecipeServiceTest.java
+++ src/test/java/project/mealPlan/service/RecipeServiceTest.java
@@ -1,24 +1,57 @@
 package project.mealPlan.service;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import project.mealPlan.entity.Meal;
+import project.mealPlan.entity.Recipe;
+import project.mealPlan.repository.*;
+import java.util.Collections;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
 class RecipeServiceTest {
+    @Mock
+    private RecipeRepository recipeRepository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private MealRepository mealRepository;
+    @Mock
+    private Recipe_IngredientRepository recipeIngredientRepository;
+    @InjectMocks
+    private RecipeService recipeService;
 
     @Test
-    void deleteRecipe() {
+    void deleteRecipe_success() {
+        Recipe recipe = new Recipe();
+        when(recipeRepository.findByRecipeId(any())).thenReturn(recipe);
+        when(userRepository.findAllByUserFavouriteRecipesContains(any())).thenReturn(Collections.emptyList());
+        when(mealRepository.findByMealRecipes(any())).thenReturn(Collections.singletonList(new Meal()));
+        ResponseEntity<?> response = recipeService.deleteRecipe(1);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Recipe deleted", response.getBody());
+        verify(recipeIngredientRepository, times(1)).deleteAll(any());
     }
-
     @Test
-    void getRecipeData() {
+    void deleteRecipe_recipeNotFound() {
+        when(recipeRepository.findByRecipeId(any())).thenReturn(null);
+        ResponseEntity<?> response = recipeService.deleteRecipe(1);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Recipe not found", response.getBody());
+        verify(recipeIngredientRepository, never()).deleteAll(any());
     }
-
     @Test
-    void addToFavourites() {
-    }
-
-    @Test
-    void getFavouriteRecipes() {
+    void updateRecipe_success() {
+        when(recipeRepository.findByRecipeId(any())).thenReturn(new Recipe());
+        ResponseEntity<?> response = recipeService.updateRecipe(Collections.singletonMap("recipeId", 1));
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Recipe updated", response.getBody());
     }
 }
