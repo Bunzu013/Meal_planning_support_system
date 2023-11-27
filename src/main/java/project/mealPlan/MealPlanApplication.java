dfd3457886afd895cabf64a5bff824207492a200
Bunzu013 <83347605+Bunzu013@users.noreply.github.com>
2023-11-27 03:06:39 +0100

    dane do testow

--- src/main/java/project/mealPlan/MealPlanApplication.java
+++ src/main/java/project/mealPlan/MealPlanApplication.java
@@ -2,10 +2,13 @@ package project.mealPlan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@ComponentScan(basePackages = {"project.mealPlan.controller", "project.mealPlan.configuration", "project.mealPlan.service", "project.mealPlan.repository"})
+
 public class MealPlanApplication {
 
 	public static void main(String[] args) {
