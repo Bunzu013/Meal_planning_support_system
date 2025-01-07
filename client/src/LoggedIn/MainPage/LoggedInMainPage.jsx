230a7a565899bd8b40b2276c6abf3d8fbe9b2055
Baran Julia <83347605+Bunzu013@users.noreply.github.com>
2025-01-07 20:25:08 +0100

    refactor: format code

--- client/src/LoggedIn/MainPage/LoggedInMainPage.jsx
+++ client/src/LoggedIn/MainPage/LoggedInMainPage.jsx
@@ -1,14 +1,14 @@
-import { React, useEffect, useState }  from 'react';
+import { React, useEffect, useState } from 'react';
 import '../Styles/MainPage.css'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
-function LoggedInMainPage(){
-    const [favoriteRecipes, setFavoriteRecipes] = useState([]);
-    const [recommendedRecipes, setRecommendedRecipes] = useState([]);
+function LoggedInMainPage() {
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const [recommendedRecipes, setRecommendedRecipes] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
       try {
         const recipesResponse = await axios.get(`http://localhost:8080/getAllRecipes`);
@@ -25,7 +25,7 @@ function LoggedInMainPage(){
     };
 
     fetchData();
-   
+
   }, [recommendedRecipes]);
 
   const randomRecipes = (recipes) => {
@@ -69,42 +69,42 @@ function LoggedInMainPage(){
           console.error('Error adding recipe to favorites', error);
         });
     }
-}
-    
-    return(
-        <div className="Page">
-            <div className='body'>
-                <div className="container body-content col-10">
-                    <h1 className='main-title'>MEAL IDEAS</h1>
-                    <p className='text'>Our recipe recommendations just for you.</p>  
-                    <div className="container">
-                        <div className="flex-row d-flex flex-wrap justify-content-center">
-                            {recommendedRecipes.map((recipe, index) => (
-                                <div key={index} className="col-12 col-md-12 col-lg-3 col-sm-12 recipe">
-                                    <div className="text-center">
-                                        <img src={`data:image/jpeg;base64, ${recipe.imageData}`} style={{'height': '185px'}} alt="recipe.imageName" className="img-fluid recipe-img"></img>
-                                        <button type="button" className={`heart-icon ${isRecipeInFavorites(recipe.recipeId) ? 'favorite' : ''}`} onClick={() => handleAddToFavorites(recipe.recipeId)}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill={isRecipeInFavorites(recipe.recipeId) ? '#ef6767' : '#fff'} stroke="#ef6767" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-                                            </svg>
-                                        </button>
-                                        <h3 className='recipe-title'>{recipe.recipeName}</h3>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                    <div className="item-right">
-                        <Link to="/recipes">
-                            <button className="btn button-main">
-                                MORE
-                            </button>  
-                        </Link>
-                    </div>
-                </div>      
-            </div>     
+  }
+
+  return (
+    <div className="Page">
+      <div className='body'>
+        <div className="container body-content col-10">
+          <h1 className='main-title'>MEAL IDEAS</h1>
+          <p className='text'>Our recipe recommendations just for you.</p>
+          <div className="container">
+            <div className="flex-row d-flex flex-wrap justify-content-center">
+              {recommendedRecipes.map((recipe, index) => (
+                <div key={index} className="col-12 col-md-12 col-lg-3 col-sm-12 recipe">
+                  <div className="text-center">
+                    <img src={`data:image/jpeg;base64, ${recipe.imageData}`} style={{ 'height': '185px' }} alt="recipe.imageName" className="img-fluid recipe-img"></img>
+                    <button type="button" className={`heart-icon ${isRecipeInFavorites(recipe.recipeId) ? 'favorite' : ''}`} onClick={() => handleAddToFavorites(recipe.recipeId)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill={isRecipeInFavorites(recipe.recipeId) ? '#cf598e' : '#fff'} stroke={isRecipeInFavorites(recipe.recipeId) ? '#cf598e' : '#088F8F'} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                      </svg>
+                    </button>
+                    <h3 className='recipe-title'>{recipe.recipeName}</h3>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="item-right">
+            <Link to="/recipes">
+              <button className="btn button-main">
+                MORE
+              </button>
+            </Link>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default LoggedInMainPage;
