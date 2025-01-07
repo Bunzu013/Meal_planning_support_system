5eb3ae7e79a30a0fec2a43f224e78dd09165a2cc
Baran Julia <83347605+Bunzu013@users.noreply.github.com>
2025-01-07 20:21:47 +0100

    feat: change from pink to blue color palet

--- client/src/icons/SearchIcon.jsx
+++ client/src/icons/SearchIcon.jsx
@@ -3,7 +3,7 @@ import './Icons.css';
 
 function SearchIcon(){
     return(
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ec8a8a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#088F8F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <circle cx="11" cy="11" r="8" />
             <line x1="21" y1="21" x2="16.65" y2="16.65" />
         </svg>
